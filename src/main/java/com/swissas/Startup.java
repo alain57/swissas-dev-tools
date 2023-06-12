a32734d8c4d48c0578441c9e79b66463e926a484
jangzn <79316320+jangzn@users.noreply.github.com>
2023-06-12 09:13:47 +0200

    Fix usage of deprecated StartupActivity.Background

--- src/main/java/com/swissas/Startup.java
+++ src/main/java/com/swissas/Startup.java
@@ -2,21 +2,26 @@ package com.swissas;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.util.messages.MessageBus;
 import com.intellij.util.messages.MessageBusConnection;
 import com.swissas.actions_on_save.Processor;
 import com.swissas.actions_on_save.SaveActionManager;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.intellij.AppTopics.FILE_DOCUMENT_SYNC;
 
-public class Startup implements StartupActivity.Background {
+public class Startup implements ProjectActivity {
 	
+	@Nullable
 	@Override
-	public void runActivity(@NotNull Project project) {
+	public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
 		registerSaveActionStuff();
-		
+		return null;
 	}
 	
 	public void registerSaveActionStuff() {
