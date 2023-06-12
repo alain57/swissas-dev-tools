f4a24b112b5953748edcb0be92f455ea280a90d3
jangzn <79316320+jangzn@users.noreply.github.com>
2023-06-12 09:10:41 +0200

    Use method to access static property

--- src/main/java/com/swissas/toolwindow/adapters/WarningContentKeyAdapter.java
+++ src/main/java/com/swissas/toolwindow/adapters/WarningContentKeyAdapter.java
@@ -4,9 +4,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Optional;
 
-import com.intellij.openapi.util.Key;
-import com.intellij.ui.SpeedSearchBase;
-import com.intellij.ui.TreeSpeedSearch;
+import com.intellij.ui.speedSearch.SpeedSearchSupply;
 import com.intellij.ui.treeStructure.Tree;
 import com.swissas.toolwindow.WarningContentTreeNode;
 
@@ -18,7 +16,6 @@ import com.swissas.toolwindow.WarningContentTreeNode;
  */
 public class WarningContentKeyAdapter extends KeyAdapter {
 	private final Tree tree;
-	private static final Key SPEED_SEARCH_COMPONENT_MARKER = Key.findKeyByName("SPEED_SEARCH_COMPONENT_MARKER");//not nice but SpeedSearchSupply.SPEED_SEARCH_COMPONENT_MARKER is private :/
 	
 	public WarningContentKeyAdapter(Tree tree) {
 		this.tree = tree;
@@ -27,9 +24,7 @@ public class WarningContentKeyAdapter extends KeyAdapter {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		super.keyPressed(e);
-		Boolean speedSearchVisible = Optional.ofNullable(this.tree.getClientProperty(SPEED_SEARCH_COMPONENT_MARKER))
-				.map(TreeSpeedSearch.class::cast)
-				.map(SpeedSearchBase::isPopupActive).orElse(false);
+		boolean speedSearchVisible = Optional.ofNullable(SpeedSearchSupply.getSupply(this.tree)).map(SpeedSearchSupply::isPopupActive).orElse(false);
 		WarningContentTreeNode selectedNode = (WarningContentTreeNode) this.tree
 				.getLastSelectedPathComponent();
 		if (selectedNode != null && !speedSearchVisible && (e.getKeyCode() == KeyEvent.VK_DELETE
